b3f9f5671044f0986cc37e13ba0bd4c4b41d5166
Wesley Schreur <info@wesleyschreur.nl>
2025-09-20 21:15:00 +0200

    new file for permissions plugin

---
+++ carbon/plugins/rec_permissions.cs
@@ -0,0 +1,13 @@
+//! https://github.com/SN1P3S101
+
+namespace Carbon.Plugins {
+
+    [Info("rec_permissions", "SN1P3S_", "0.0.1")]
+    [Description("Permissions, groups, ranks and tags management system.")]
+    public class rec_permissions : CarbonPlugin {
+
+        
+
+    }
+
+}
